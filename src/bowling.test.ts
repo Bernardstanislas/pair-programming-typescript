8648228cbaf50447abb8d0afdb23b4adae542756
Sammy Teillet <sammy.teillet@gmail.com>
2019-07-17 11:33:48 +0200

    Initial commit

---
+++ src/bowling.test.ts
@@ -0,0 +1,14 @@
+import  {Bowling} from './bowling'
+
+describe('Bowling', () => {
+  it('handles a very boring game', () => {
+    const bowling = new Bowling();
+
+    for(let i = 0; i<20; i++) {
+      bowling.roll(0);
+    }
+
+    expect(bowling.getScore()).toBe(0);
+  });
+
+});
---
+++ src/bowling.ts
@@ -0,0 +1,17 @@
+export const sum
+  = (...a: number[]) =>
+    a.reduce((acc, val, croute) => acc + val, 0);
+
+export class Bowling {
+  constructor() {
+
+  }
+
+  roll(pins: number) {
+
+  }
+
+  getScore() {
+
+  }
+}
