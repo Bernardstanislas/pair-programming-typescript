d9efbf75c2080c4c04b9083dfa616cf3a3cc7208
Stanislas Bernard <bernardstanislas@gmail.com>
2019-07-23 11:39:19 +0200

    :ok_hand: Apply @Samox changes

--- src/bowling.test.ts
+++ src/bowling.test.ts
@@ -4,9 +4,26 @@ describe("Bowling", () => {
   it("handles a very boring game", () => {
     const bowling = new Bowling();
 
-    for (let i = 0; i < 20; i++) {
-      bowling.roll(0);
-    }
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
+    bowling.roll(0);
 
     expect(bowling.getScore()).toBe(0);
   });
